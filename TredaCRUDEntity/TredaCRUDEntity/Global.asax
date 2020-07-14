694a0f18eb6b440edbfe6f209e20e43ffffd02e7
Jan Michael Sanchez Sierra <maicol_931028@hotmail.com>
2020-07-14 00:47:09 -0500

    Se agregan las clases modelos a migrar en ingles

---
+++ TredaCRUDEntity/TredaCRUDEntity/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="TredaCRUDEntity.MvcApplication" Language="C#" %>
---
+++ TredaCRUDEntity/TredaCRUDEntity/Models/Stores.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TredaCRUDEntity.Models
+{
+    public class Stores
+    {
+        public int StoreId { get; set; }
+        public string StoreName { get; set; }
+        public DateTime OpeningDate { get; set; }
+    }
+}
